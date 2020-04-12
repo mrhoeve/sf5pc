e8c299f914961cf81f996cd649b1a8a619de2946
Marc Hoeve <mrhoeve@gmail.com>
2020-04-12 23:42:37 +0200

    Added vets list and updates controller. Closes #15

--- sf5pc-web/src/main/java/nl/hicts/petclinic/controller/VetController.java
+++ sf5pc-web/src/main/java/nl/hicts/petclinic/controller/VetController.java
@@ -1,14 +1,21 @@
 package nl.hicts.petclinic.controller;
 
+import lombok.RequiredArgsConstructor;
+import nl.hicts.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
+@RequiredArgsConstructor
 public class VetController {
+	private final VetService vetService;
 	
 	@RequestMapping({"/", "", "/index", "/index.html"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("vets", vetService.findAll());
+		
 		return "vets/index";
 	}
 }
