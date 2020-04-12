bc4cbefa0e7aed066cfcad1ef1415636d6c62a49
Marc Hoeve <mrhoeve@gmail.com>
2020-04-12 16:17:46 +0200

    Adding owners page and controller. Closes #13

---
+++ sf5pc-web/src/main/java/nl/hicts/petclinic/controller/OwnerController.java
@@ -0,0 +1,13 @@
+package nl.hicts.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+	
+	@RequestMapping({"/owners", "/owners/", "/owners/index", "/owners/index.html"})
+	public String index() {
+		return "owners/index";
+	}
+}
