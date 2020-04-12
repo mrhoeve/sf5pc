5848557758f135bc4c70227739a87ae74451ee3f
Marc Hoeve <mrhoeve@gmail.com>
2020-04-12 23:35:50 +0200

    Added owners list and updated controller. Closes #16

--- sf5pc-web/src/main/java/nl/hicts/petclinic/controller/OwnerController.java
+++ sf5pc-web/src/main/java/nl/hicts/petclinic/controller/OwnerController.java
@@ -1,14 +1,21 @@
 package nl.hicts.petclinic.controller;
 
+import lombok.RequiredArgsConstructor;
+import nl.hicts.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
+@RequiredArgsConstructor
 public class OwnerController {
+	private final OwnerService ownerService;
 	
 	@RequestMapping({"/", "", "/index", "/index.html"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
+		
 		return "owners/index";
 	}
 }
