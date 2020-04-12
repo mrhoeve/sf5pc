375c3c2659afc6f32ff8b9a4ebd3b531988f02ca
Marc Hoeve <mrhoeve@gmail.com>
2020-04-12 16:20:33 +0200

    Changed mapping on controllers for clearity

--- sf5pc-web/src/main/java/nl/hicts/petclinic/controller/VetController.java
+++ sf5pc-web/src/main/java/nl/hicts/petclinic/controller/VetController.java
@@ -3,10 +3,11 @@ package nl.hicts.petclinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 	
-	@RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html"})
+	@RequestMapping({"/", "", "/index", "/index.html"})
 	public String index() {
 		return "vets/index";
 	}
