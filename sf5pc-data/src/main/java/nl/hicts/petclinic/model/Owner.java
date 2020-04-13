9ab26a0c7f27359a90f1a68c5dcfdfbc5ea30b2e
Marc Hoeve <mrhoeve@gmail.com>
2020-04-13 19:40:48 +0200

    Added contact info to owner, closes #27.

--- sf5pc-data/src/main/java/nl/hicts/petclinic/model/Owner.java
+++ sf5pc-data/src/main/java/nl/hicts/petclinic/model/Owner.java
@@ -8,5 +8,14 @@ import java.util.Set;
 public class Owner extends Person {
 	@Getter
 	@Setter
+	private String address;
+	@Getter
+	@Setter
+	private String city;
+	@Getter
+	@Setter
+	private String telephone;
+	@Getter
+	@Setter
 	private Set<Pet> pets;
 }
