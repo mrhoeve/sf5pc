b8a3491965045effde1756f5749e24588ed40f4a
Marc Hoeve <mrhoeve@gmail.com>
2020-04-14 23:47:23 +0200

    Convert vets to JPA entities, closes #36

--- sf5pc-data/src/main/java/nl/hicts/petclinic/model/Specialty.java
+++ sf5pc-data/src/main/java/nl/hicts/petclinic/model/Specialty.java
@@ -3,6 +3,11 @@ package nl.hicts.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 	@Getter
 	@Setter
