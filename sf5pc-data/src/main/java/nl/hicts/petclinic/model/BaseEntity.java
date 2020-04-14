fcc5db27d4e3fdb5247d84cd024f3fb46d7429b0
Marc Hoeve <mrhoeve@gmail.com>
2020-04-14 23:25:26 +0200

    Implement base entity, closes #34

--- sf5pc-data/src/main/java/nl/hicts/petclinic/model/BaseEntity.java
+++ sf5pc-data/src/main/java/nl/hicts/petclinic/model/BaseEntity.java
@@ -3,10 +3,17 @@ package nl.hicts.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	@Getter
 	@Setter
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 }
