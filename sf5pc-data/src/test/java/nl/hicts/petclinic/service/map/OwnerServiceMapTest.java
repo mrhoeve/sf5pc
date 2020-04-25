91120d9974fdb2a8f0136036b4913e2a9f60eb38
Marc Hoeve <mrhoeve@gmail.com>
2020-04-25 16:46:50 +0200

    Added tests, closes #49

---
+++ sf5pc-data/src/test/java/nl/hicts/petclinic/service/map/OwnerServiceMapTest.java
@@ -0,0 +1,73 @@
+package nl.hicts.petclinic.service.map;
+
+import nl.hicts.petclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerServiceMapTest {
+	OwnerServiceMap ownerServiceMap;
+	Long ownerId = 1L;
+	String lastName = "lastName";
+	
+	@BeforeEach
+	void setUp() {
+		ownerServiceMap = new OwnerServiceMap(new PetTypeServiceMap(), new PetServiceMap());
+		ownerServiceMap.save(Owner.builder().id(ownerId).lastName(lastName).build());
+	}
+	
+	@Test
+	void findAll() {
+		Set<Owner> ownerSet = ownerServiceMap.findAll();
+		assertEquals(1, ownerSet.size());
+	}
+	
+	@Test
+	void findById() {
+		Owner owner = ownerServiceMap.findById(ownerId);
+		assertEquals(1L, owner.getId());
+	}
+	
+	@Test
+	void saveExistingId() {
+		long id = 2L;
+		Owner owner2 = Owner.builder().id(id).build();
+		Owner savedOwner = ownerServiceMap.save(owner2);
+		assertEquals(id, savedOwner.getId());
+	}
+	
+	@Test
+	void saveWithoutId() {
+		Owner owner2 = Owner.builder().build();
+		Owner savedOwner = ownerServiceMap.save(owner2);
+		assertNotNull(savedOwner.getId());
+	}
+	
+	@Test
+	void delete() {
+		ownerServiceMap.delete(ownerServiceMap.findById(ownerId));
+		assertEquals(0, ownerServiceMap.findAll().size());
+	}
+	
+	@Test
+	void deleteById() {
+		ownerServiceMap.deleteById(ownerId);
+		assertEquals(0, ownerServiceMap.findAll().size());
+		
+	}
+	
+	@Test
+	void findByLastName() {
+		Owner owner = ownerServiceMap.findByLastName(lastName);
+		assertEquals(lastName, owner.getLastName());
+	}
+	
+	@Test
+	void findByLastNameNotFound() {
+		Owner owner = ownerServiceMap.findByLastName("foo");
+		assertNull(owner);
+	}
+}
